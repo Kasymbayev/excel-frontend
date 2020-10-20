1377f687ff40ce13fb003f4dbb228dc9d2681f2d
y.kassymbayev <y.kassymbayev>
2020-10-21 05:22:17 +0600

    completed:(framework-base-functional)

---
+++ src/core/DOMListener.js
@@ -0,0 +1,35 @@
+import {capitalize} from '@core/utils';
+
+export class DOMListener {
+  constructor($root, listeners = []) {
+    if (!$root) {
+      throw new Error(`No $root provided for DOMListener`)
+    }
+    this.$root = $root
+    this.listeners = listeners
+  }
+
+  initDOMListeners() {
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener)
+      if (!this[method]) {
+        const name = this.name || ''
+        // eslint-disable-next-line max-len
+        throw new Error(`Method ${method} is not implemented in ${name} Component`)
+      }
+      this[method] = this[method].bind(this)
+      this.$root.on(listener, this[method])
+    })
+  }
+
+  removeDOMListeners() {
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener)
+      this.$root.off(listener, this[method])
+    })
+  }
+}
+
+function getMethodName(eventName) {
+  return 'on' + capitalize(eventName)
+}
