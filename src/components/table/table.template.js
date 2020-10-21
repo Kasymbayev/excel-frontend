2e2cbaf9e1797aa4ce53ae9aba4d24b31b930155
y.kassymbayev <y.kassymbayev>
2020-10-21 20:21:30 +0600

    completed:(table_resize)

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,17 +3,24 @@ const CODES = {
   Z: 90
 }
 
-function toCell() {
-  return `<div class="cell" contenteditable></div>`
+function toCell(_, col) {
+  return `<div class="cell" contenteditable data-col="${col}"></div>`
 }
 
-function toColumn(col) {
-  return `<div class="column">${col}</div>`
+function toColumn(col, index) {
+  return `<div class="column" data-type="resizable" data-col="${index}">
+            ${col}
+            <div class="col-resize" data-resize="col"></div>
+          </div>`
 }
 
 function createRow(content, index) {
-  return `<div class="row">
-    <div class="row-info">${index ? index : ''}</div>
+  const resize = index ? '<div class="row-resize" data-resize="row"></div>' : ''
+  return `<div class="row" data-type="resizable">
+    <div class="row-info">
+         ${index ? index : ''}
+         ${resize}
+    </div>
     <div class="row-data">${content}</div>
   </div>`
 }
